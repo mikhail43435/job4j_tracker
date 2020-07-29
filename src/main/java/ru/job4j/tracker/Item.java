ab14086532c4575ec2d1275a1af0dee8b4fa436a
mikhail43435 <hommp2009@mail.ru>
2020-07-29 16:44:38 +0300

    Тестирование. Подготовка данных. [#325162]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,13 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
